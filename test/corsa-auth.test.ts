892438b02bb5ef1c996a30812e5d38ba08737cf1
lukemccrae <lukemccrae@gmail.com>
2023-10-21 10:13:06 -0400

    Initial commit

---
+++ test/corsa-auth.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CorsaAuth from '../lib/corsa-auth-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/corsa-auth-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CorsaAuth.CorsaAuthStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
